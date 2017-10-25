c9f402b3bcfa19b70af2f3b1fb741cc5a2a94273
Alexander Londoño Espejo <alexlondon07@secuencia24.com>
2017-10-24 21:00:23 -0500

    Longitude and latitude

--- app/src/main/java/io/github/alexlondon07/googlemaps/MainActivity.java
+++ app/src/main/java/io/github/alexlondon07/googlemaps/MainActivity.java
@@ -1,20 +1,130 @@
 package io.github.alexlondon07.googlemaps;
 
+import android.annotation.TargetApi;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import io.github.alexlondon07.googlemaps.services.LocationServices;
+
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int ALL_PERMISSION_RESULT = 101;
+    private LocationServices locationServices;
+
+    private ArrayList<String> permissionToRequest;
+    private ArrayList<String> permissionToRejected = new ArrayList<>();
+    private ArrayList<String> permissions = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Permission
+        permissions.add(ACCESS_FINE_LOCATION);
+        permissions.add(ACCESS_COARSE_LOCATION);
+        permissionToRequest = findUnAskedPermissions(permissions);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(permissionToRequest.size() > 0){
+                requestPermissions(permissionToRequest.toArray(new String[permissionToRequest.size()]), ALL_PERMISSION_RESULT);
+            }
+        }
+
+        locationServices = new LocationServices(MainActivity.this);
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case ALL_PERMISSION_RESULT:
+                for(String perms: permissionToRequest){
+                    if(hasPermission(perms)){
+                        permissionToRejected.add(perms);
+                    }
+                }
+                if(permissionToRejected.size() > 0){
+                    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                        if(shouldShowRequestPermissionRationale(permissionToRejected.get(0))){
+                            showMessageOkCancel("This permission is required to run this application",
+                                    new DialogInterface.OnClickListener(){
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                                                requestPermissions(permissionToRejected.toArray( new String[permissionToRejected.size()]), ALL_PERMISSION_RESULT);
+                                            }
+                                        }
+                                    });
+                        }
+                    }
+                }
+        }
+    }
+
+    private void showMessageOkCancel(String message, DialogInterface.OnClickListener onClickListener) {
+
+        new AlertDialog.Builder(MainActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", onClickListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
+
+    private ArrayList<String> findUnAskedPermissions(ArrayList<String> permissions) {
+        ArrayList<String> results = new ArrayList<>();
+        for (String wanted: permissions){
+            if(!hasPermission(wanted)){
+                results.add(wanted);
+            }
+        }
+        return results;
+    }
+
+    private boolean hasPermission(String wanted) {
+        if(canMakeMatch()){
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                return (checkSelfPermission(wanted) == PackageManager.PERMISSION_GRANTED);
+            }
+        }
+        return true;
+    }
+
+    private boolean canMakeMatch() {
+        return (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1);
     }
 
     public void launchMap(View view) {
         Intent intent = new Intent(MainActivity.this, MapsActivity.class);
         startActivity(intent);
     }
+
+    public void getLocationDevice(View view){
+        if(locationServices.isCanGetLocation()){
+            double longitude = locationServices.getLongitude();
+            double latitude = locationServices.getLatitude();
+
+            Toast.makeText(MainActivity.this, "Longitude " + longitude + " Latitude " + latitude, Toast.LENGTH_LONG).show();
+
+        }else{
+            //TODO SHOW CONFIGURATION ALERT
+            locationServices.showSettingsAlert();
+        }
+    }
+
 }
---
+++ app/src/main/java/io/github/alexlondon07/googlemaps/services/LocationServices.java
@@ -0,0 +1,161 @@
+package io.github.alexlondon07.googlemaps.services;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Service;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+
+/**
+ * Created by alexlondon07 on 10/24/17.
+ */
+
+public class LocationServices extends Service implements LocationListener{
+
+    private final Context context;
+    private LocationManager locationManager;
+    private boolean checkGPS = false;
+    private boolean checkNetwork = false;
+    private boolean canGetLocation= false;
+
+
+    private static final long MIN_DISTANCE_FOR_UPDATE = 10;
+    private  static  final long MIN_TIME_FOR_UPDATES = 1000 * 60 * 1;
+    private Location location;
+    private String TAG = "LocationServices";
+    private double latitude;
+    private double longitude;
+
+    public LocationServices(Context context) {
+        this.context = context;
+        getLocation();
+    }
+
+    private void getLocation(){
+        try {
+
+            locationManager = (LocationManager) context.getSystemService(LOCATION_SERVICE);
+
+            //Get state GPS
+            checkGPS = locationManager.isProviderEnabled(locationManager.GPS_PROVIDER);
+
+            //Get state internet
+            checkNetwork = locationManager.isProviderEnabled(locationManager.NETWORK_PROVIDER);
+
+            if(!checkGPS && !checkNetwork){
+                //TODO add alert or toast notification
+                canGetLocation = false;
+            }else{
+                canGetLocation = true;
+
+                //check GPS Location
+                if(checkGPS){
+                    if(ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
+                            != PackageManager.PERMISSION_GRANTED
+                            && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION)
+                            != PackageManager.PERMISSION_GRANTED) {
+                        //TODO Implements dialog with permission
+                    }
+                    locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER, MIN_TIME_FOR_UPDATES, MIN_DISTANCE_FOR_UPDATE, this);
+
+                    location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                }
+
+                //check Network
+                if(checkNetwork){
+                    if(ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
+                            != PackageManager.PERMISSION_GRANTED
+                            && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION)
+                            != PackageManager.PERMISSION_GRANTED) {
+                        //TODO Implements dialog with permission
+                    }
+                    locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER, MIN_TIME_FOR_UPDATES, MIN_DISTANCE_FOR_UPDATE, this);
+
+                    location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                    if(location != null){
+                        latitude = location.getLatitude();
+                        longitude = location.getLongitude();
+                        Log.e(TAG, "line 89");
+                    }
+
+                }
+            }
+
+        }catch (Exception e){
+            Log.e(TAG, e.getMessage());
+        }
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public boolean isCanGetLocation() {
+        return canGetLocation;
+    }
+
+
+    //Permission settings
+    public void showSettingsAlert(){
+        AlertDialog.Builder alerDialog = new AlertDialog.Builder(context);
+        alerDialog.setTitle("GPS no enabled");
+        alerDialog.setMessage("You want to turn it on?");
+        alerDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                context.startActivity(intent);
+            }
+        });
+        alerDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+        alerDialog.show();
+    }
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
